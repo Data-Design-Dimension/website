6c338acff32058685d33ed6d0aae881bd043d9df
Kathryn Hurchla <khurchla@users.noreply.github.com>
2021-09-01 22:06:24 -0400

    added HTML template and routed test

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 app= Flask(__name__)
 
+#@app.route("/")
+#def dadeda():
+#    return "Data Design Dimension"
+
 @app.route("/")
-def dadeda():
-    return "Data Design Dimension"
+def template_test():
+    return render_template('template.html', my_string="Wheeeee!", my_list=[0,1,2,3,4,5])
+
 
 if __name__=="__main__":
     app.run(debug= True)
