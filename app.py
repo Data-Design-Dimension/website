85fc58d9297766fe55a5031f88bdb9214f4ffb52
Kathryn Hurchla <kathryn@dadeda.design>
2022-03-09 15:28:26 -0500

    Added 2 process pages; refined text

--- app.py
+++ app.py
@@ -9,13 +9,17 @@ def home():
 def work():
    return render_template("work.html")
 
+@app.route("/invest-as-one")
+def invest_as_one():
+   return render_template("invest-as-one.html")
+
 @app.route("/freedom-map")
 def freedom_map():
    return render_template("freedom-map.html")
 
 @app.route("/about")
 def about():
-   return render_template("about.html", my_string="compel action with data backed visual storytelling.", my_list1=['Web applications'], my_list2=['Interactive and static charts'], my_list3=['Maps'])
+   return render_template("about.html", my_string="data writing, products, and solutions.", my_list1=['Data science blogs'], my_list2=['Data visualization tutorials'], my_list3=['Visual communications'], my_list4=['Analytic web applications'], my_list5=['Maps'], my_list6=['Interactive charts'])
 
 @app.route("/contact")
 def contact():
@@ -26,4 +30,4 @@ def benefit():
    return render_template("benefit.html", my_string="public benefit created.")
 
 if __name__=="__main__":
-    app.run(debug= False)
+    app.run(debug= True)
